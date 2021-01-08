9820a46d8844523e3fd1584efa0188abb6047a8b
Fred Nixon <john@johnfnixon.com>
2021-01-08 08:50:53 -0500

    early experiment

--- dtest.py
+++ dtest.py
@@ -9,12 +9,18 @@ possible_drives = [
 
 SECTOR_SIZE = 512
 BUFFER_SIZE = 1024*SECTOR_SIZE
-buf = bytearray(BUFFER_SIZE)
+buf1 = bytearray(BUFFER_SIZE)
+buf2 = bytearray(BUFFER_SIZE)
 
 for drive in possible_drives:
     try:
         fd = os.open(drive, os.O_RDONLY)
-        ### os.write(fd, buf)
-        os.read(fd, 16)
-    except:
-        pass
+        br = 1
+
+        while br > 0:
+            br = os.readv(fd, [buf1, buf2])
+            btotal += br
+            print(">> read {btotal} bytes", end='\r')
+
+    except IOError as error:
+        print(error)
