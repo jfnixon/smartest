89303ecb336d42b1287fc1c98c56d03295098773
Fred Nixon <john@johnfnixon.com>
2021-04-04 22:44:32 -0400

    Start organizing the effort

--- dtest.rb
+++ dtest.rb
@@ -9,6 +9,15 @@
 # We'll have the same problem here as we do in python. We need to be grated
 # full disk access for the interpreter.
 #
+# Same sort of algorithm. Lay out the main structure.
+
+# Process arge
+
+# Print info
+
+# Open file
+
+# Begin read loop
 
 if
 end
